e910396a90fb02c1dc1ab0fdca3c61cd9e675a75
103048116 <37065822+ailuenchen@users.noreply.github.com>
2018-04-10 23:54:51 +0800

    first commit

--- hw4_fibo.py
+++ hw4_fibo.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-def compute_fibonacci(num):
-    # your code here ...
-    return ...
-
-
+fibo_list = [0, 1]
 num = int(input("你想看費氏數列第幾個數字？ "))
-print('費氏數列第', num, '個數字是', compute_fibonacci(num))
+
+for i in range(2, num):
+    num = fibo_list[-1] + fibo_list[-2]
+    fibo_list.append(num)
+
+print('費氏數列第', num, '個數字是', fibo_list[-1])
